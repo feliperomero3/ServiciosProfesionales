7638cc6986e5d34115b4f9778d217e3180e6c5e5
Felipe Romero <feliperomero3@outlook.com>
2018-05-31 11:21:24 -0600

    Aplicar formato a Web.config y actualizar datos del Ensamblado.

--- ServiciosProfesionales.Web/Properties/AssemblyInfo.cs
+++ ServiciosProfesionales.Web/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ServiciosProfesionales.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("ServiciosProfesionales.Web")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")]
+[assembly: AssemblyCompany("Felipe Romero")]
+[assembly: AssemblyProduct("Servicios Profesionales")]
+[assembly: AssemblyCopyright("Copyright © Felipe Romero 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
