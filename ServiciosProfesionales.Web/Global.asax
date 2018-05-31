ab47bf24afef1a4fd78832cc3c3c7ede77172b11
Felipe Romero <feliperomero3@outlook.com>
2018-05-31 08:05:46 -0600

    Add project files.

---
+++ ServiciosProfesionales.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiciosProfesionales.Web.MvcApplication" Language="C#" %>
---
+++ ServiciosProfesionales.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiciosProfesionales.Web.Startup))]
+namespace ServiciosProfesionales.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
