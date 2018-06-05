e973b8d8620da75beb50752c1333195645142b32
Felipe Romero <feliperomero3@outlook.com>
2018-06-04 18:49:47 -0600

    Refactorizar ManageController.

--- ServiciosProfesionales.Web/Identity/ApplicationSignInManager.cs
+++ ServiciosProfesionales.Web/Identity/ApplicationSignInManager.cs
@@ -10,8 +10,8 @@ namespace ServiciosProfesionales.Web.Identity
     // Configure the application sign-in manager which is used in this application.
     public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
     {
-        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
-            : base(userManager, authenticationManager)
+        public ApplicationSignInManager(ApplicationUserManager userManager,
+            IAuthenticationManager authenticationManager) : base(userManager, authenticationManager)
         {
         }
 
