7f60fb80fa2be028a328c73745f5c94d9f98171c
Felipe Romero <feliperomero3@outlook.com>
2018-08-27 16:50:33 -0600

    Remover paquete GitVersionTask.

--- ServiciosProfesionales.Web/Properties/AssemblyInfo.cs
+++ ServiciosProfesionales.Web/Properties/AssemblyInfo.cs
@@ -30,7 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-//
-// GitVersonTask is automatically handling versioning of the project.
-// http://gitversion.readthedocs.io/en/latest/usage/msbuild-task/
-//
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
