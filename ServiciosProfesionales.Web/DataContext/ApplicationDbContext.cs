b6ad7bb897f57eb694ccb72c30c553988bee3edb
Felipe Romero <feliperomero3@outlook.com>
2018-09-03 17:19:56 -0600

    Personalizar nombres de tablas generadas de Identity.

--- ServiciosProfesionales.Web/DataContext/ApplicationDbContext.cs
+++ ServiciosProfesionales.Web/DataContext/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
-using ServiciosProfesionales.Web.Identity;
 using ServiciosProfesionales.Web.Models;
 
 namespace ServiciosProfesionales.Web.DataContext
@@ -19,6 +18,13 @@ namespace ServiciosProfesionales.Web.DataContext
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+
+            modelBuilder.Entity<IdentityUser>().ToTable("AspNetUser", "Identity");
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRole", "Identity");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("AspNetUserLogin");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("AspNetUserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("AspNetUserClaim", "Identity");
+
         }
     }
 }
