308649b1c05af84d503e5f53a6dc4e70370b0bed
Felipe Romero <feliperomero3@outlook.com>
2019-03-28 17:53:08 -0700

    Agregar propiedad Total en Ingreso.

--- ServiciosProfesionales/Entities/Ingreso.cs
+++ ServiciosProfesionales/Entities/Ingreso.cs
@@ -12,6 +12,7 @@ namespace ServiciosProfesionales.Entities
         public decimal Iva16 { get; set; }
         public decimal Iva10 { get; set; }
         public decimal Isr10 { get; set; }
+        public decimal Total { get; set; }
 
         public Servicio Servicio { get; set; }
         public Factura Factura { get; set; }
