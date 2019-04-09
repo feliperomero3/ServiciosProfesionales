1330e7c31ef42b41a6f1a63f44e6a0fb7c0bb588
Felipe Romero <feliperomero3@outlook.com>
2019-04-09 17:50:26 -0600

    Agregar entidad Gasto.

---
+++ ServiciosProfesionales/Entities/Gasto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ServiciosProfesionales.Entities
+{
+    public class Gasto
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Importe { get; set; }
+        public decimal Iva { get; set; }
+        public decimal Total { get; set; }
+    }
+}
