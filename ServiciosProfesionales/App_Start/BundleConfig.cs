b008f9259c8eac1a82585640f0cab450b3f067a1
Felipe Romero <feliperomero3@outlook.com>
2019-04-17 08:49:34 -0600

    Aplicar estilo de 'bootstrap-theme'.

--- ServiciosProfesionales/App_Start/BundleConfig.cs
+++ ServiciosProfesionales/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace ServiciosProfesionales
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
